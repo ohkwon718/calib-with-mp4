e4939b0bb2185910463a39b48e149deb90de0279
Oh-Hun Kwon <kirloyal@gmail.com>
2018-07-08 19:22:06 +0200

    undistort and projection

--- calib.py
+++ calib.py
@@ -10,8 +10,7 @@ nCorners = (5,7)
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 30, 0.001)
 
 objp = np.zeros((nCorners[0]*nCorners[1],3), np.float32)
-objp[:,:2] = np.mgrid[0:nCorners[0],0:nCorners[1]].T.reshape(-1,2)
-# print objp
+objp[:,:2] = np.mgrid[0:nCorners[0],0:nCorners[1]].T.reshape(-1,2) * 0.0288
 
 objpoints = [] # 3d point in real world space
 imgpoints = [] # 2d points in image plane.
@@ -22,8 +21,10 @@ cap = cv2.VideoCapture('video/cam1.MP4')
 if (cap.isOpened()== False): 
     print("Error opening video stream or file")
 
-nFrame4calib = 3
+nFrame4calib = 30
 nFrameTotal = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+w = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+h = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 if nFrameTotal <= nFrame4calib:
     idxs = np.arange(nFrameTotal)
@@ -71,25 +72,95 @@ while(cap.isOpened()):
 cap.release()
 cv2.destroyAllWindows()
 
-
 cali_ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
 
 
-for frame, rvec, tvec, corners in zip(frames, rvecs, tvecs, imgpoints):
-    rot, _ = cv2.Rodrigues(rvec)
-    x = np.dot(rot,objp.T) + tvec
-    x = x / x[2,:]
-    x = np.dot(mtx,x)[:2,:]
-    print np.max(np.abs(x - corners[:,0,:].T)) # maximum diff btw projected and original corner
+img = frames[0]
+h,  w = img.shape[:2]
+newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
-    for p in x.T:
-        cv2.circle(frame, tuple(p.astype(int)), 1, (0,0,255), 3)
+print newcameramtx
+print roi
+
+# undistort
+dst = cv2.undistort(img, mtx, dist, None, newcameramtx)
+ 
+# # undistort
+# mapx,mapy = cv2.initUndistortRectifyMap(mtx,dist,None,newcameramtx,(w,h),5)
+# dst = cv2.remap(img,mapx,mapy,cv2.INTER_LINEAR)
+ 
+# crop the image
+x,y,w,h = roi
+# dst = dst[y:y+h, x:x+w]
+
+tot_error = 0
+mean_error = 0
+# for i in xrange(len(objpoints)):
+#     imgpoints2, _ = cv2.projectPoints(objpoints[i], rvecs[i], tvecs[i], mtx, dist)
+#     error = cv2.norm(imgpoints[i],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+#     tot_error += error
+
+objp = objpoints[0]
+rvec = rvecs[0]
+tvec = tvecs[0]
+
+
+imgpoints2, _ = cv2.projectPoints(objp, rvec, tvec, newcameramtx, dist)
+# imgpoints2, _ = cv2.projectPoints(objp, rvec, tvec, newcameramtx, np.zeros(5))
+
+for p in imgpoints2[:,0,:]:
+    cv2.circle(dst, tuple(p.astype(int)), 1, (0,0,255), 3)
+
+error = cv2.norm(imgpoints[0],imgpoints2, cv2.NORM_L2)/len(imgpoints2)
+tot_error += error
+print "total error: ", tot_error/len(objpoints)
+
+cv2.imshow('Frame',frame)
+cv2.imshow('Dst',dst)
+cv2.waitKey(0)
+
+
+
+
+# # for frame, rvec, tvec, corners in zip(frames, rvecs, tvecs, imgpoints):
+# #     rot, _ = cv2.Rodrigues(rvec)
+# #     x = np.dot(rot,objp.T) + tvec
+# #     x = x / x[2,:]
+# #     # c = (1 + )
+# #     # * x[0,:] 
+# #     diffX = (x[0,:] - w/2)
+# #     diffY = (x[1,:] - h/2)
+# #     r = np.sqrt(diffX**2 + diffY**2)
+# #     c = 
+
+# #     x = np.dot(mtx,x)[:2,:]
+# #     print np.max(np.abs(x - corners[:,0,:].T)) # maximum diff btw projected and original corner
+    
+# #     for p in x.T:
+# #         cv2.circle(frame, tuple(p.astype(int)), 1, (0,0,255), 3)
     
 
-    cv2.imshow('Frame',frame)
+# #     cv2.imshow('Frame',frame)
 
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
-       break
+# #     if cv2.waitKey(1000) & 0xFF == ord('q'):
+# #        break
+
+
+# # # for frame, rvec, tvec, corners in zip(frames, rvecs, tvecs, imgpoints):
+# # #     rot, _ = cv2.Rodrigues(rvec)
+# # #     x = np.dot(rot,objp.T) + tvec
+# # #     x = x / x[2,:]
+# # #     x = np.dot(mtx,x)[:2,:]
+# # #     print np.max(np.abs(x - corners[:,0,:].T)) # maximum diff btw projected and original corner
+
+# # #     for p in x.T:
+# # #         cv2.circle(frame, tuple(p.astype(int)), 1, (0,0,255), 3)
+    
+
+# # #     cv2.imshow('Frame',frame)
+
+# # #     if cv2.waitKey(1000) & 0xFF == ord('q'):
+# # #        break
 
 
 
